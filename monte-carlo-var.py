90e1b8c8c4202b531e05ab6b368146017926b58a
sverma13 <57202681+sverma13@users.noreply.github.com>
2020-02-29 17:26:15 -0600

    Update monte-carlo-var.py

--- monte-carlo-var.py
+++ monte-carlo-var.py
@@ -1,93 +1,97 @@
-# Data analysis libraries
+# Import libraries
 import pandas as pd
 import numpy as np
-
-# Data visualization library
 import matplotlib.pyplot as plt
 
+# Select ticker of interest
+ticker = 'TSLA'
 
-# Function: Compute a stock's VaR, CVar, and Monte Carlo VaR
-def stock_analysis(ticker):
+# Load the data
+ticker_file = ticker + '.csv'
+data = pd.read_csv(ticker_file, parse_dates=['Date'])
+data = data.sort_values(by='Date')
+data.set_index('Date', inplace=True)
+data['Returns'] = data['Adj Close'].pct_change()  # Create daily returns column
+returns = data['Returns'].dropna()  # Remove values of N/A
 
-    # Load data in pandas dataframe
-    ticker_file = ticker + '.csv'
-    data = pd.read_csv(ticker_file, parse_dates=['Date'])
-    data = data.sort_values(by='Date')
-    data.set_index('Date', inplace=True)
+# Compute statistical data
+avg_daily_return = np.mean(returns)  # mean of daily returns
+std_dev = np.std(returns)  # standard deviation of daily returns
 
-    data['Returns'] = data['Adj Close'].pct_change()  # Create daily returns column
-    returns = data['Returns'].dropna()  # Remove values of N/A
+# Calculate VaR 95 and CVaR 95
+var95 = np.percentile(returns, 5)
+print('var: %0.4f' % var95)
+cvar95 = returns[returns <= var95].mean()
+print('cvar: %0.4f' % cvar95)
 
-    # Compute statistical data
-    avg_daily_return = np.mean(returns)
-    std_dev = np.std(returns)
+# Calculate drawdown
+cum_rets_dollars = data['Adj Close']
+running_max = cum_rets_dollars.cummax()
+drawdown = (cum_rets_dollars) / running_max - 1
 
-    # Calculate VaR 95 and CVaR 95
-    var95 = np.percentile(returns, 5)
-    print('var:', var95)
-    cvar95 = returns[returns <= var95].mean()
-    print('cvar:', cvar95)
-    
-    # Calculate drawdown and plot figures
-    cum_rets_dollars = data['Adj Close']
-    print('cum_rets:')
-    print(cum_rets_dollars)
+# Plot drawdown
+plt.figure(figsize=(10,10))
+plt.subplot(3,1,1)
+plt.title('Cumulative Returns')
+plt.xlabel('Date')
+plt.ylabel('Price ($)')
+cum_rets_dollars.plot()
+plt.tight_layout()
 
-    running_max = cum_rets_dollars.cummax()
-    print('running_max:')
-    print(running_max)
+plt.subplot(3,1,2)
+plt.title('Running Max')
+plt.xlabel('Date')
+plt.ylabel('Price ($)')
+running_max.plot()
 
-    drawdown = (cum_rets_dollars) / running_max - 1
-    print('drawdown:')
-    print(drawdown)
+plt.subplot(3,1,3)
+drawdown.plot()
+plt.title('Drawdown')
+plt.xlabel('Date')
+plt.ylabel('Drawdown (%)')
+drawdown.plot()
+plt.tight_layout()
+plt.show()
 
-    plt.figure(figsize=(8, 7))
-    plt.subplot(2,1,1)
-    plt.plot(cum_rets_dollars)
-    plt.title('Cumulative Returns')
-    plt.subplot(2,1,2)
-    plt.plot(running_max)
-    plt.title('Running Max')
-    plt.show()
-    
-    plt.figure()
-    plt.plot(drawdown)
-    plt.title('Drawdown')
-    plt.show()
+# Define constants
+T = 252  # Time period
+S0 = cum_rets_dollars.iloc[-1]  # most recent stock price
+NUM_SIM = 1000  # Number of Monte Carlo simulations
 
-    T = 252  # Time period
-    S0 = 1  # Starting stock price
-    #S0 = cum_rets_dollars.iloc[-1]  # use this line to have the Monte Carlo simulation start at the most recent stock price
-    NUM_SIM = 1000  # Number of Monte Carlo simulations
-    for i in range(NUM_SIM):
-        rand_rets = np.random.normal(avg_daily_return, std_dev, T)  # Generate a random daily stock return using the stock's historical statistical data
+# Re-compute VaR and CVaR using only data from the most recent T days
+returns_comp = returns.tail(T)
+var95 = np.percentile(returns_comp, 5)
+print('var: %0.4f' % var95)
+cvar95 = returns_comp[returns_comp <= var95].mean()
+print('cvar: %0.4f' % cvar95)
 
-        # Create cumulative returns data
-        cum_rets = rand_rets + 1
-        forecast = S0 * (cum_rets.cumprod())
+# Monte Carlo simulation
+for i in range(NUM_SIM):
+    rand_rets = np.random.normal(avg_daily_return, std_dev, T)  # Generate a random daily stock return using the stock's historical statistical data
 
-        # Plot the Monte Carlo simulations over time
-        plt.plot(forecast)
-        plt.title('Monte Carlo Simulation - Stock Price Over Time')
-        plt.xlabel('Time (Days)')
-        plt.ylabel('Stock Price (Percent of initial stock price)')
+    # Create cumulative returns data
+    cum_rets = rand_rets + 1
+    forecast = S0 * (cum_rets.cumprod())
 
-    # Calculate Monte Carlo VaR from the simulations
-    var = np.percentile(rand_rets, 5)
-    print('Monte Carlo var:', var)
-    plt.show()
-    
-    # Create a histogram of daily stock returns
-    plt.hist(returns, bins=25)
-    plt.title('Daily Returns Frequency Over %d Days' % T)
-    plt.xlabel('Daily Returns (Percent)')
-    plt.ylabel('Frequency')
-    plt.axvline(x=var95, color='green')  # plot the historical VaR on the histogram
-    plt.axvline(x=cvar95, color='red')  # plot the historical CVar on the histogram
-    plt.axvline(x=var, color='black', linewidth=2)  # plot the Monte Carlo VaR on the histogram
-    plt.show()
+    # Plot the Monte Carlo simulations over time
+    plt.plot(forecast)
+    plt.title('Monte Carlo Simulation - Stock Price Over Time')
+    plt.xlabel('Time (Days)')
+    plt.ylabel('Stock Price (Percent of initial stock price)')
+plt.show()
 
-    return var  # return the Monte Carlo Var
+# Calculate Monte Carlo VaR from the simulations
+var = np.percentile(rand_rets, 5)
+print('Monte Carlo var: %0.4f' % var)
 
-
-monte_carlo_var = stock_analysis('TSLA')
+# Create a histogram of daily stock returns
+plt.figure(figsize=(8,6))
+plt.hist(returns_comp, bins=25)
+plt.title('Daily Returns Frequency Over %d Days' % T)
+plt.xlabel('Daily Returns (Percent)')
+plt.ylabel('Frequency')
+plt.axvline(x=var95, color='green', label='VaR 95: %0.4f' % var95)  # plot the historical VaR on the histogram
+plt.axvline(x=cvar95, color='red', label='CVaR 95: %0.4f' % cvar95)  # plot the historical CVar on the histogram
+plt.axvline(x=var, color='black', linewidth=2, label='Monte Carlo VaR 95: %0.4f' % var)  # plot the Monte Carlo VaR on the histogram
+plt.legend()
+plt.show()
